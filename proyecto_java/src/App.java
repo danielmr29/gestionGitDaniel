72250c33fc53a78c381ef38d719978ecea6ce447
DESKTOP-RK5IK49\dmram <danielmr@iescastelar.com>
2023-09-26 10:39:45 +0200

    no message

--- proyecto_java/src/App.java
+++ proyecto_java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Errores en los bucles");
+        System.out.println("Errores en los bucles ");
     }
 }
