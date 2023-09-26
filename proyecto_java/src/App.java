eed43cfcb31aafb07a72c0465e9fb1da55668a2c
DESKTOP-RK5IK49\dmram <danielmr@iescastelar.com>
2023-09-26 10:31:17 +0200

    Creaccion rama marcas.

--- proyecto_java/src/App.java
+++ proyecto_java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Creacción de la rama feature/test");
+        System.out.println("Creacción de la rama feature/marcas");
     }
 }
