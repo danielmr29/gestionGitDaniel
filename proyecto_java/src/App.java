9301f7730d3e223d85c71b683c074d33d37c8fbb
DESKTOP-RK5IK49\dmram <danielmr@iescastelar.com>
2023-09-26 10:34:37 +0200

    Correcion bucles.

--- proyecto_java/src/App.java
+++ proyecto_java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Creacción del proyecto Java");
+        System.out.println("Errores en los bucles");
     }
 }
